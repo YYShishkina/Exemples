2b80bb63eb34e67fcf458eaa967439bdb215d353
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-05 19:14:56 +0300

    Добавили пример с методом

---
+++ Example008_array_metod/Program.cs
@@ -0,0 +1,14 @@
+﻿
+        int Max(int arg1, int arg2, int arg3)
+        {
+            int result = arg1;
+            if (arg2 > result) result = arg2;
+            if (arg3 > result) result = arg3;
+            return result;
+        }
+
+        int a = 1;
+        int b = 2;
+        int c = 6;
+        int max1 = Max(a, b, c);
+        Console.WriteLine(max1);
