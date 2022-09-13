3cd358c6bb2ddf06efab88c8a6307a164e92fb5f
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2022-09-13 17:31:09 +0300

    Добавлены примеры из лекции 2

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+double numberA = 30;
+double numberB = 4;
+double result = numberA / numberB;
+Console.WriteLine(result);
---
+++ Example005_if/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+String username = Console.ReadLine();
+
+if (username.ToLower () == "маша")
+{ 
+    Console.WriteLine ("Ура, этоже Маша!");
+}
+else
+{
+    Console.WriteLine ("Привет, ");
+    Console.WriteLine (username);
+}
